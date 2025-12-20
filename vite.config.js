e45036a0d406a66d6e3ccae8c3baa3ff61b88318
halizadz <halizasukarno9@gmail.com>
2025-12-21 02:28:11 +0700

    portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
+  base: '/',  // Penting untuk Vercel
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    port: 3000
   }
 })
